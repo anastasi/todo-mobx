9b282805b16d79bb985e0039411ec8d127b064e9
Anastasia Yanvareva <anastasia.yanvareva@gmail.com>
2018-05-28 23:14:11 +0200

    less indents

--- src/List/List.js
+++ src/List/List.js
@@ -4,15 +4,15 @@ import {inject, observer} from 'mobx-react'
 @inject('TodoList')
 @observer
 class List extends Component {
-    render() {
-        const {TodoList} = this.props
-    
-        return (
-          <div className="List">
-             <ul>{TodoList.lists.map(item => <li key={item}>{item}</li>)}</ul>
-          </div>
-        );
-      }
+  render() {
+    const {TodoList} = this.props
+
+    return (
+      <div className="List">
+        <ul>{TodoList.lists.map(item => <li key={item}>{item}</li>)}</ul>
+      </div>
+    );
+  }
 }
 
 export default List
