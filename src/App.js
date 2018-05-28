fb639f634ba85b7850be9181f64274b899ef9dad
Anastasia Yanvareva <anastasia.yanvareva@gmail.com>
2018-05-28 23:03:46 +0200

    now we can add ToDos

--- src/App.js
+++ src/App.js
@@ -3,7 +3,13 @@ import './App.css'
 import {inject, observer} from 'mobx-react'
 
 @inject('TodoList')
+@observer
 class App extends Component {
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.TodoList.addList(this.todo.value);
+    e.target.reset();
+  }
   render() {
     const {TodoList} = this.props
 
@@ -11,7 +17,12 @@ class App extends Component {
       <div className="App">
          <h2>
            You have {TodoList.listCount} todo!
+           <form onSubmit={ this.handleSubmit }>
+            <input type="text" placeholder="New todo" ref={ input => this.todo = input} />
+            <button>Add</button>
+           </form>
          </h2>
+         <ul>{TodoList.lists.map(item => <li key={item}>{item}</li>)}</ul>
       </div>
     );
   }
