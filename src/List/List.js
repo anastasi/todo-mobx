c5e4ec510e92bf7aea56809f595fb2e58e4688bf
Anastasia Yanvareva <anastasia.yanvareva@gmail.com>
2018-05-30 23:10:26 +0200

    set complete count. WIP

--- src/List/List.js
+++ src/List/List.js
@@ -4,13 +4,19 @@ import {inject, observer} from 'mobx-react'
 @inject('TodoList')
 @observer
 class List extends Component {
+
+  handleChange = e => {
+    e.preventDefault();
+    //this.props.TodoList.addList(this.todo.value);
+    // this.props.TodoList.completeList(0)
+  }
   render() {
     const {TodoList} = this.props
 
     return (
       <div className="List">
         <ul>{TodoList.lists.map(
-            item => <li key={item.name}><input type="checkbox" ref={ input => this.todo = input} />{item.name}</li>
+            item => <li key={item.name}><input type="checkbox" onChange={this.handleChange} />{item.name}</li>
           )}</ul>
       </div>
     );
